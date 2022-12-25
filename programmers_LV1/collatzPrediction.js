67b5ea57fd9319a1cf39e6415316234af3bdcebe
dev-jq1 <dev.jq1@gmail.com>
2022-12-25 23:55:47 +0900

    콜라츠 추측

---
+++ programmers_LV1/collatzPrediction.js
@@ -0,0 +1,12 @@
+// !Level  : 1
+// ?문제명 : 콜라츠 추측
+
+function solution(num) {
+    if(num === 1) return 0;
+    for(let i = 0; i < 500; i++){
+        if(num === 1) return i;
+        if(num % 2 === 0) num = num / 2;
+        else num = num * 3 + 1;
+    }
+    return -1;
+}
