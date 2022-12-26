4075eb7862dbd9a8dfaaf0b97e22da8549cdd85c
dev-jq1 <dev.jq1@gmail.com>
2022-12-26 20:51:35 +0900

    저주의 숫자 3 풀이변경

--- programmers_LV0/curseNumThree.js
+++ programmers_LV0/curseNumThree.js
@@ -2,19 +2,11 @@
 // 문제명 : 저주의 숫자 3
 
 function solution(n) {
-
     var answer = [];
-
-    while(true){
-        for(let i = 1; ;i++ ){
-            if(String(i).includes("3") || i % 3 === 0){
-                continue;
-            }else{
-                answer.push(i);
-            }
-            if(answer.length === n) return answer[n-1];
-        }
+    for (let i = 1; ; i++) {
+        if (String(i).includes("3") || i % 3 === 0) continue;
+        else answer.push(i);
+        if (answer.length === n) return answer[n - 1];
     }
 }
-
-console.log(String(3).includes("4"))
+console.log(String(3).includes("4"));
