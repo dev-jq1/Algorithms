d856720eea3655d5803f98928e9ca8edf8cff09a
dev-jq1 <dev.jq1@gmail.com>
2022-12-30 23:57:29 +0900

    고차함수 풀이

--- programmers_LV0/babbling1.js
+++ programmers_LV0/babbling1.js
@@ -5,25 +5,39 @@ function solution(babbling) {
     answer = 0;
     const baby = ["aya", "ye", "woo", "ma"];
     // let tempStr = "";
-    for(let i = 0; i < babbling.length; i++){
+    for (let i = 0; i < babbling.length; i++) {
         let tempStr = babbling[i];
         let rpCnt = 0;
-        for(let j = 0; j < baby.length; j++){
-            if(tempStr.includes(baby[j])){
-                tempStr = tempStr.replace(baby[j],"_");
+        for (let j = 0; j < baby.length; j++) {
+            if (tempStr.includes(baby[j])) {
+                tempStr = tempStr.replace(baby[j], "_");
                 rpCnt++;
             }
         }
 
         let compareStr = "";
-        for(let k = 0; k< rpCnt; k++){
+        for (let k = 0; k < rpCnt; k++) {
             compareStr += "_";
         }
-        
-        if(tempStr === compareStr){
+
+        if (tempStr === compareStr) {
             answer++;
             // tempStr = "";
         }
     }
     return answer;
+}
+
+function solution2(babbling) {
+    let words = ["aya", "ye", "woo", "ma"];
+    let newArr = [];
+    babbling.forEach((x) => {
+        words.forEach((y) => {
+            if (x.includes(y)) {
+                x = x.replace(y, 1);
+                newArr.push(x);
+            }
+        });
+    });
+    return newArr.filter((el) => !isNaN(Number(el))).length;
 }
