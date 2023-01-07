e6efc282bb8d4e6d09c9019a7423b26cc09cd265
dev-jq1 <dev.jq1@gmail.com>
2023-01-07 23:51:52 +0900

    [1차] 다트 게임

---
+++ programmers_LV1/[1차]다트게임.js
@@ -0,0 +1,11 @@
+// !Level  : 1
+// ?문제명 : [1차] 다트 게임
+// !2018 KAKAO BLIND RECRUITMENT
+
+function solution(dartResult) {
+    let first = 0;
+    let second = 0;
+    let third = 0;
+    dartResult.replace(/[A-Z]/g," ")
+    
+}
